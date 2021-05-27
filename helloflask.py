87489eb9401bc02815929c70c5e5ba43f7c123f8
Soulmate7 <1012194891@qq.com>
2021-05-27 20:20:11 +0800

    first commit

---
+++ helloflask.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template
+import cv2
+import dlib
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    outname=detect('static/101.png')
+    return render_template('index.html',outname=outname)
+
+def detect(filename):
+    img=cv2.imread(filename)
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    #人脸分类器
+    detector=dlib.get_frontal_face_detector()
+    #获取人脸检测器
+    predictor=dlib.shape_predictor(r"/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/shape_predictor_68_face_landmarks.dat")
+    #dets保存的是图像中人脸的矩形框，可以有多个
+    dets=detector(gray,1)
+    
+    for face in dets:
+        shape=predictor(img,face)# 寻找人脸的68个标定点
+        # 遍历所有点，打印出其坐标，并圈出来
+        for pt in shape.parts():
+            pt_pos=(pt.x,pt.y)
+            cv2.circle(img,pt_pos,2,(0,255,0),1)
+    outname='static/face.png'
+    cv2.imwrite(outname,img)
+    return outname
+
+if __name__=='__main__':
+    app.run()
---
+++ index.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+import cv2
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    outname=detect('static/101.png')
+    return render_template('index.html',outname=outname)
+
+def detect(filename):
+    face_cascade = cv2.CascadeClassifier('/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+
+    img = cv2.imread(filename)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    outname='static/101.png'
+    cv2.imwrite(outname,img)
+    return outname
+
+
+if __name__=='__main__':
+    app.run()
